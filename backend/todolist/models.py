34fae6268fada50aed00307cfe46aa1592811efa
NickMakeThing <67160596+NickMakeThing@users.noreply.github.com>
2020-09-17 22:06:12 +1000

    omitted reminders

--- backend/todolist/models.py
+++ backend/todolist/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 import random
 from django.contrib.auth.models import User
-# Create your models here.#
-def randomColour(): #'pink'
+
+def randomColour(): 
     return random.choice(['#333399','purple','#00d6bd','#a90a0a','#40cc3e','#df7334','#a9993c'])
 
 class List(models.Model):
     userid = models.ForeignKey(User, on_delete=models.CASCADE)
     listName = models.CharField(max_length=25)
     colour = models.CharField(max_length=21, default=randomColour)
-    #
     
 class Task(models.Model):
     listId = models.ForeignKey('List',on_delete=models.CASCADE)
